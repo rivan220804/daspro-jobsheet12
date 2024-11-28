4bea28b3220f18d2b02aba3db62c73dcf9e88630
rivan220804 <fadillahrivan25@gmail.com>
2024-11-28 09:40:53 +0700

    Percobaan1_23

---
+++ Percobaan2_23.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Percobaan2_23 {
+
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.print("1");
+            return 1;
+        } else {
+            System.out.printf("%dx", x);
+            return (x * hitungPangkat(x, y-1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bilangan yang dihitung: ");
+        int bilangan = sc.nextInt();
+        System.out.print("Pangkat: ");
+        int pangkat = sc.nextInt();
+
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.printf(" = %d", hasil);
+    }
+}
