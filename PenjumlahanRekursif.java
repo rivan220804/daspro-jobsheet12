989178ead50111408e765648b0a1e30d91622248
rivan220804 <fadillahrivan25@gmail.com>
2024-11-28 10:27:22 +0700

    PenjumlahanRekursif23

---
+++ PenjumlahanRekursif.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PenjumlahanRekursif {
+
+    static int penjumlahanRekursif(int f) {
+        if (f == 0) {
+            return 0;
+        } else {
+            return f + penjumlahanRekursif(f - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan f: ");
+        int f = sc.nextInt();
+
+        int hasil = penjumlahanRekursif(f);
+        System.out.println("Penjumlahan dari 1 sampai " + f + " adalah: " + hasil);
+    }
+}
